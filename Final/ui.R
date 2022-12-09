9bf16892baf7f46aa7ba8d92f40d9de462537537
Melanie Kahn <mmkahn@ncsu.edu>
2022-12-09 13:50:59 -0500

    adding tabs

--- Final/ui.R
+++ Final/ui.R
@@ -10,48 +10,177 @@ library(DT)
 sheet1 <- read_csv("https://www4.stat.ncsu.edu/~online/datasets/EDU01a.csv")
 
 # Define UI for application
-shinyUI(fluidPage(
+shinyUI(navbarPage("Final Project",
   
-  # Application title
-  titlePanel("Summaries for German Credit Data"),
+  # Create first tab: About page
+  tabPanel("About",
   
-  # Create Sidebar
-  sidebarLayout(
-    # Customize Sidebar
-    sidebarPanel(
-      # Third level header
-      h3("This data set comes from the", a(href = "https://topepo.github.io/caret/", target="_blank", "caret package"),"- originally from the UCI machine learning repository"),
+      # Create Sidebar
+      sidebarLayout(
+        # Customize Sidebar
+        sidebarPanel(
+          # Third level header
+          h3("This data set comes from the", a(href = "https://topepo.github.io/caret/", target="_blank", "caret package"),"- originally from the UCI machine learning repository"),
       
-      # Line break
-      br(),
+          # Line break
+          br(),
       
-      # Fourth level header
-      h4("You can create a few bar plots using the radio buttons below."),
+          # Fourth level header
+          h4("You can create a few bar plots using the radio buttons below."),
       
-      # Radio button widget in sidebar
-      radioButtons("plotType", label="Select the Plot Type", choices = c("Just Classification", "Classification and Unemployed", "Classification and Foreign"), selected = "Just Classification"),
+          # Radio button widget in sidebar
+          radioButtons("plotType", label="Select the Plot Type", choices = c("Just Classification", "Classification and Unemployed", "Classification and Foreign"), selected = "Just Classification"),
       
-      # Line break
-      br(),
+          # Line break
+          br(),
       
-      # Fourth level header
-      h4("You can find the", strong("sample mean"), "for a few variables below:"), # Third level header
+          # Fourth level header
+          h4("You can find the", strong("sample mean"), "for a few variables below:"), # Third level header
       
-      # Select box widget in sidebar
-      selectInput("var", label="Select the Plot Type", choices = c("Duration", "Amount", "Age"), selected = "Age"),
+          # Select box widget in sidebar
+          selectInput("var", label="Select the Plot Type", choices = c("Duration", "Amount", "Age"), selected = "Age"),
       
-      # Numeric input widget in sidebar
-      numericInput("round", label="Select the number of digits for rounding", value = 2, min = 0, max = 5, step = 1),
-    ),
+          # Numeric input widget in sidebar
+          numericInput("round", label="Select the number of digits for rounding", value = 2, min = 0, max = 5, step = 1),
+        ),
     
-    # Customize Main panel
-    mainPanel(
-      #dataPlot is name of "plot" object in server
-      plotOutput("dataPlot"),
+        # Customize Main panel
+        mainPanel(
+          #dataPlot is name of "plot" object in server
+          plotOutput("dataPlot"),
       
-      #dataTable is name of "data" object in server
-      dataTableOutput("dataTable")
-      
-    )
-  )
+          #dataTable is name of "data" object in server
+          dataTableOutput("dataTable")
+        )
+      )
+    ),
+  
+  # Create second tab: Data Exploration page
+  tabPanel("Data Exploration",
+           
+           # Create Sidebar
+           sidebarLayout(
+             # Customize Sidebar
+             sidebarPanel(
+               # Third level header
+               h3("This data set comes from the", a(href = "https://topepo.github.io/caret/", target="_blank", "caret package"),"- originally from the UCI machine learning repository"),
+               
+               # Line break
+               br(),
+               
+               # Fourth level header
+               h4("You can create a few bar plots using the radio buttons below."),
+               
+               # Radio button widget in sidebar
+               radioButtons("plotType", label="Select the Plot Type", choices = c("Just Classification", "Classification and Unemployed", "Classification and Foreign"), selected = "Just Classification"),
+               
+               # Line break
+               br(),
+               
+               # Fourth level header
+               h4("You can find the", strong("sample mean"), "for a few variables below:"), # Third level header
+               
+               # Select box widget in sidebar
+               selectInput("var", label="Select the Plot Type", choices = c("Duration", "Amount", "Age"), selected = "Age"),
+               
+               # Numeric input widget in sidebar
+               numericInput("round", label="Select the number of digits for rounding", value = 2, min = 0, max = 5, step = 1),
+             ),
+             
+             # Customize Main panel
+             mainPanel(
+               #dataPlot is name of "plot" object in server
+               plotOutput("dataPlot"),
+               
+               #dataTable is name of "data" object in server
+               dataTableOutput("dataTable")
+             )
+           )
+  ),
+  # Create third tab: Modeling page
+  tabPanel("About",
+           
+           # Create Sidebar
+           sidebarLayout(
+             # Customize Sidebar
+             sidebarPanel(
+               # Third level header
+               h3("This data set comes from the", a(href = "https://topepo.github.io/caret/", target="_blank", "caret package"),"- originally from the UCI machine learning repository"),
+               
+               # Line break
+               br(),
+               
+               # Fourth level header
+               h4("You can create a few bar plots using the radio buttons below."),
+               
+               # Radio button widget in sidebar
+               radioButtons("plotType", label="Select the Plot Type", choices = c("Just Classification", "Classification and Unemployed", "Classification and Foreign"), selected = "Just Classification"),
+               
+               # Line break
+               br(),
+               
+               # Fourth level header
+               h4("You can find the", strong("sample mean"), "for a few variables below:"), # Third level header
+               
+               # Select box widget in sidebar
+               selectInput("var", label="Select the Plot Type", choices = c("Duration", "Amount", "Age"), selected = "Age"),
+               
+               # Numeric input widget in sidebar
+               numericInput("round", label="Select the number of digits for rounding", value = 2, min = 0, max = 5, step = 1),
+             ),
+             
+             # Customize Main panel
+             mainPanel(
+               #dataPlot is name of "plot" object in server
+               plotOutput("dataPlot"),
+               
+               #dataTable is name of "data" object in server
+               dataTableOutput("dataTable")
+             )
+           )
+  ),
+  
+  # Create forth tab: Data page
+  tabPanel("Data",
+           
+           # Create Sidebar
+           sidebarLayout(
+             # Customize Sidebar
+             sidebarPanel(
+               # Third level header
+               h3("This data set comes from the", a(href = "https://topepo.github.io/caret/", target="_blank", "caret package"),"- originally from the UCI machine learning repository"),
+               
+               # Line break
+               br(),
+               
+               # Fourth level header
+               h4("You can create a few bar plots using the radio buttons below."),
+               
+               # Radio button widget in sidebar
+               radioButtons("plotType", label="Select the Plot Type", choices = c("Just Classification", "Classification and Unemployed", "Classification and Foreign"), selected = "Just Classification"),
+               
+               # Line break
+               br(),
+               
+               # Fourth level header
+               h4("You can find the", strong("sample mean"), "for a few variables below:"), # Third level header
+               
+               # Select box widget in sidebar
+               selectInput("var", label="Select the Plot Type", choices = c("Duration", "Amount", "Age"), selected = "Age"),
+               
+               # Numeric input widget in sidebar
+               numericInput("round", label="Select the number of digits for rounding", value = 2, min = 0, max = 5, step = 1),
+             ),
+             
+             # Customize Main panel
+             mainPanel(
+               #dataPlot is name of "plot" object in server
+               plotOutput("dataPlot"),
+               
+               #dataTable is name of "data" object in server
+               dataTableOutput("dataTable")
+             )
+           )
+  ),
+  
 ))
